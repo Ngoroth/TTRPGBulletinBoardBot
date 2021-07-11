80f91bcd8a21cb5e7f792aaf07a378209acdffdc
Ngoroth <ngoroth@yandex.ru>
2021-07-11 13:03:45 +0300

    Fix saving date game bug. Refactoring

--- src/TTRPGBulletinBoardBot.Core/Services/BotService.cs
+++ src/TTRPGBulletinBoardBot.Core/Services/BotService.cs
@@ -27,6 +27,7 @@ namespace TTRPGBulletinBoardBot.Core.Services
             if (userMessage.StartsWith("/find_players"))
                 return resetToStage(userEntity, Stage.AskGameName);
 
+            UserEntity updatedUserEntity;
             switch (userEntity.Stage)
             {
                 case Stage.Publication:
@@ -37,40 +38,46 @@ namespace TTRPGBulletinBoardBot.Core.Services
                 case Stage.AskDescription:
                 case Stage.AskExpectations:
                 case Stage.AskSystem:
-                    return processAskStage(userEntity, userMessage);
-                case Stage.AskDateTime:
+                    updatedUserEntity = setAnswer(userEntity, userMessage);
                     return new[]
-                        {
-                            (BotAction.SendMessageToUser, TextFormat.MarkdownV2,
-                                _publicationService.MakePublicationText(userEntity))
-                        }
-                        .Concat(processAskStage(userEntity, userMessage));
-                case Stage.Preview:
-                    if (userMessage.ToLower().StartsWith("да"))
                     {
-                        return processAskStage(userEntity, userMessage)
-                            .Concat(new[]
-                            {
-                                (BotAction.MakePublicationInChannel, TextFormat.MarkdownV2,
-                                    _publicationService.MakePublicationText(userEntity))
-                            });
-                    }
+                        (BotAction.SendMessageToUser, TextFormat.Text,
+                            _phraseService.GetPhrase(updatedUserEntity.Stage))
+                    };
+                case Stage.AskDateTime:
+                    updatedUserEntity = setAnswer(userEntity, userMessage);
+                    return new[]
+                    {
+                        (BotAction.SendMessageToUser, TextFormat.MarkdownV2,
+                            _publicationService.MakePublicationText(updatedUserEntity)),
+                        (BotAction.SendMessageToUser, TextFormat.Text,
+                            _phraseService.GetPhrase(updatedUserEntity.Stage))
+                    };
+                case Stage.Preview:
+                    if (!userMessage.ToLower().StartsWith("да"))
+                        return resetToStage(userEntity, Stage.Start);
 
-                    return resetToStage(userEntity, Stage.Start);
+                    updatedUserEntity = setAnswer(userEntity, userMessage);
+                    return new[]
+                    {
+                        (BotAction.SendMessageToUser, TextFormat.Text,
+                            _phraseService.GetPhrase(updatedUserEntity.Stage)),
+                        (BotAction.MakePublicationInChannel, TextFormat.MarkdownV2,
+                            _publicationService.MakePublicationText(updatedUserEntity))
+                    };
+
                 default:
                     return new[]
                         {(BotAction.SendMessageToUser, TextFormat.Text, _phraseService.GetPhrase(Stage.Start))};
             }
         }
 
-        private IEnumerable<(BotAction BotAction, TextFormat TextFormat, string BotMessage)> processAskStage(
-            UserEntity userEntity, string answer)
+        private UserEntity setAnswer(UserEntity userEntity, string answer)
         {
             var updatedUserEntity = userEntity with {Stage = userEntity.Stage + 1};
             updatedUserEntity.Answers[userEntity.Stage] = answer;
             _usersRepository.Update(updatedUserEntity);
-            return new[]
-                {(BotAction.SendMessageToUser, TextFormat.Text, _phraseService.GetPhrase(updatedUserEntity.Stage))};
+            return updatedUserEntity;
         }
 
         private IEnumerable<(BotAction BotAction, TextFormat TextFormat, string BotMessage)> resetToStage(
